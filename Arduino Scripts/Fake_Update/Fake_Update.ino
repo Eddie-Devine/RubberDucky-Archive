62d88b6147a8783a2a0ab838251b16a2110c0871
Eddie Devine <mr.reverse49@gmail.com>
2020-12-29 17:42:44 -0800

    New payload: Fake Update

---
+++ Arduino Scripts/Fake_Update/Fake_Update.ino
@@ -0,0 +1,44 @@
+/**
+ * Made with Duckuino, an open-source project.
+ * Check the license at 'https://github.com/Dukweeno/Duckuino/blob/master/LICENSE'
+ */
+
+#include "Keyboard.h"
+
+void typeKey(uint8_t key)
+{
+  Keyboard.press(key);
+  delay(50);
+  Keyboard.release(key);
+}
+
+/* Init function */
+void setup()
+{
+  // Begining the Keyboard stream
+  Keyboard.begin();
+
+  // Wait 500ms
+  delay(500);
+
+  // AUTHOR: Eddie Devine
+  // TARGET: Windows 10
+  delay(1000);
+  Keyboard.press(KEY_LEFT_GUI);
+  Keyboard.press('r');
+  Keyboard.releaseAll();
+
+  delay(500);
+  Keyboard.print(F("cmd /c start https://updatefaker.com/windows10/index.html"));
+
+  typeKey(KEY_RETURN);
+
+  delay(1000);
+  typeKey(KEY_F11);
+
+  // Ending stream
+  Keyboard.end();
+}
+
+/* Unused endless loop */
+void loop() {}
